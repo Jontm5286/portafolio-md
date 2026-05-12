2618136599b81723099618347fec50ed2d370ede
Jontm5286 <johantavarez89@gmail.com>
2026-05-12 17:26:41 -0400

    refactor: centralize icon library
    - Create centralized icon library at src/components/icons/
      - CalendarIcon
      - DownloadIcon
      - LinkedinIcon
      - MailIcon
      - MoonIcon
      - SunIcon
    - Update Icon.astro to use the centralized library
    - Icons can be imported individually for better tree-shaking

---
+++ src/components/icons/index.ts
@@ -0,0 +1,8 @@
+export { default as Calendar } from "./Calendar.astro";
+export { default as Download } from "./Download.astro";
+export { default as Linkedin } from "./Linkedin.astro";
+export { default as Mail } from "./Mail.astro";
+export { default as Moon } from "./Moon.astro";
+export { default as Sun } from "./Sun.astro";
+
+export type IconName = "calendar" | "download" | "linkedin" | "mail" | "moon" | "sun";
